9b24e8829539b11a6a855c71690b921dcec36b45
Navid Bin Wasim <navidbinwasim@gmail.com>
2026-02-09 15:36:29 +0600

    proper app

--- index.js
+++ index.js
@@ -6,11 +6,29 @@ let button = document.getElementById("subbtn");
 button.onclick = function(){
     let theNumber = input.value;
     theNumber = Number(theNumber)
-    if(theNumber % 2 == 0){
-        output.textContent = "even"
+    
+    output.classList.remove("even","odd", "invalid");
+
+    if(input.value.trim() === ""){
+        output.textContent = "Please enter a number";
+        output.classList.add("invalid");
+    }
+    else if(isNaN(theNumber)){
+        output.textContent = "Invalid number!";
+        output.classList.add("invalid");
+    }
+    else if(theNumber % 2 === 0){
+        output.textContent ="even";
+        output.classList.add("even")
     }
     else{
-        output.textContent = "Odd"
+        output.textContent ="odd";
+        output.classList.add("odd")
     }
-}
+
+    output.style.opacity = 0;
+    setTimeout(()=> {
+        output.style.opacity = 1;
+    }, 50);
+};
 
